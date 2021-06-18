ce762f3c849cc765c1741f824147784e71a861a4
mengzonefire <mengzonefire@qq.com>
2021-06-19 00:06:48 +0800

    update

--- P站显示百科按钮/P站显示百科按钮.user.js
+++ P站显示百科按钮/P站显示百科按钮.user.js
@@ -8,26 +8,27 @@
 // @require           https://cdn.staticfile.org/jquery/1.10.2/jquery.min.js
 // @run-at            document-start
 // ==/UserScript==
- 
-!function() {
+
+!function () {
     'use strict';
     var href = "";
     var html_code = "";
     var link = "";
     let loop = setInterval(() => {
-        if (window.location.href!=href){
-        href = window.location.href;
-        link = href.match(/\/tags\/([^\/]+)\/?/)[1];
-        html_code = `<button aria-disabled="false" class="sc-13xx43k-0 sc-13xx43k-1 kzKWyw" onclick="window.open('https://dic.pixiv.net/a/${link}','_self')">Pixiv百科全书</button>`;
-        set_btn();
+        if (window.location.href != href) {
+            href = window.location.href;
+            link = href.match(/\/tags\/([^\/]+)\/?/)[1];
+            html_code = `<button aria-disabled="false" class="sc-13xx43k-0 sc-13xx43k-1 kzKWyw" onclick="window.open('https://dic.pixiv.net/a/${link}','_self')">Pixiv百科全书</button>`;
+            set_btn();
         }
     }, 1000);
- 
-    function set_btn(){
-    let loop = setInterval(() => {
-        var html_tag = $("div.rr60ze-3.foZeuY");
-        if (!html_tag.length) return false;
-        html_tag.append(html_code);
-        clearInterval(loop);
-    }, 500);}
+
+    function set_btn() {
+        let loop = setInterval(() => {
+            var html_tag = $("div.rr60ze-3.foZeuY");
+            if (!html_tag.length) return false;
+            html_tag.append(html_code);
+            clearInterval(loop);
+        }, 500);
+    }
 }();
---
+++ P站显示百科按钮/README.md
@@ -0,0 +1,13 @@
+爪机版pixiv的tag页面(`https://www.pixiv.net/tags/`)是有百科全书跳转按钮的
+
+但PC版页面却没有:-( ,安装这个脚本会给PC版页面也加上按钮
+
+爪机版页面(自带跳转按钮)
+
+<img src="https://pic.rmb.bdstatic.com/bjh/3251ccb01891ee4ea677e389e7f61bac.png">
+
+<br>
+
+安装脚本后的PC版页面(脚本加上了按钮)
+
+<img src="https://pic.rmb.bdstatic.com/bjh/13eee6f9cba6a3d53206a9664445c63b.png">
