da6744abadcb473d3845c9c74c95f48e69ec21ca
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2022-05-13 03:31:26 +0800

    update

--- P站显示百科按钮/P站显示百科按钮.user.js
+++ P站显示百科按钮/P站显示百科按钮.user.js
@@ -2,21 +2,23 @@
 // @name              Pixiv显示百科按钮
 // @description       显示一个打开Pixiv百科页面的按钮
 // @namespace         moe.cangku.mengzonefire
-// @version           1.0.3
+// @version           1.0.4
 // @author            mengzonefire
+// @license           MIT
 // @contributionURL   https://afdian.net/@mengzonefire
 // @match             *://www.pixiv.net/*
 // @require           https://cdn.staticfile.org/jquery/3.6.0/jquery.min.js
-// @run-at            document-end
+// @run-at            document-start
 // ==/UserScript==
 !(() => {
   "use strict";
-  const htmlTag = "div.sc-1973m3p-0.eafcux";
+  const htmlTag = "div.sc-1973m3p-0";
   var tag = "";
   function addBtn() {
     let html_tag = $(htmlTag);
     if (html_tag.length) {
-      let htmlBtn = `<button id="mzf_dic_btn" aria-disabled="false" class="sc-13xx43k-0 sc-13xx43k-1 kzKWyw" onclick="window.open('https://dic.pixiv.net/a/${tag}','_self')">Pixiv百科全书</button>`;
+      let className = html_tag[0].children[0].className;
+      let htmlBtn = `<button id="mzf_dic_btn" aria-disabled="false" class="${className}" onclick="window.open('https://dic.pixiv.net/a/${tag}','_self')">Pixiv百科全书</button>`;
       html_tag.before(htmlBtn);
     } else setTimeout(addBtn, 100);
   }
