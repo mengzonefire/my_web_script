b61e1e6e8c596b1452734f04748c03679fd1f154
mengzonefire <44224240+mengzonefire@users.noreply.github.com>
2023-01-03 22:58:59 +0800

    Update ehAutoStar.user.js
    批量评分执行过程中禁用按钮并改变按钮文字, 以提示执行状态

--- ehAutoStar/ehAutoStar.user.js
+++ ehAutoStar/ehAutoStar.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name              ehAutoStar
 // @namespace         moe.cangku.mengzonefire
-// @version           1.1.2
+// @version           1.1.3
 // @description       EH画廊自动/批量评分脚本
 // @author            mengzonefire
 // @require           https://cdn.jsdelivr.net/npm/sweetalert2@9
@@ -54,6 +54,7 @@ const defRateMap = "BlossomPlus 5";
       if (!html_tag2.length) return false;
       html_tag2.before(html_input);
       $("#autoRate").click(function () {
+        $("#autoRate").prop("value", "正在执行").prop("disabled", true);
         batchRating();
       });
       clearInterval(loop);
@@ -110,6 +111,7 @@ const defRateMap = "BlossomPlus 5";
     else {
       if (recu == star_ele.length) {
         Swal.fire(`一键评分完成，共评分${count}个画廊，失败${fail_count}个`);
+        $("#autoRate").prop("value", "一键评分").prop("disabled", false);
         return true;
       }
       if (star_ele[recu].className == "ir") {
@@ -199,12 +201,14 @@ const defRateMap = "BlossomPlus 5";
       Swal.fire(
         "api参数未记录, 请先登录并进入任意画廊, 脚本会自动记录参数, 然后再使用此功能"
       );
+      $("#autoRate").prop("value", "一键评分").prop("disabled", false);
       return false;
     }
     if (my_apiuid != Number(getCookie("ipb_member_id"))) {
       Swal.fire(
         "api参数不匹配, 请先进入任意画廊, 脚本会自动刷新参数, 然后再使用此功能"
       );
+      $("#autoRate").prop("value", "一键评分").prop("disabled", false);
       return false;
     }
     var select_ele = $("div.searchnav:first > div > select > option"); // 列表右上的模式选择框 (最小化...缩略图...扩展)
@@ -230,16 +234,20 @@ const defRateMap = "BlossomPlus 5";
         break;
       default:
         Swal.fire("一键评分失败: select元素错误");
+        $("#autoRate").prop("value", "一键评分").prop("disabled", false);
+        return false;
     }
 
     if (link_ele.length != star_ele.length) {
-      Swal.fire("一键评分失败：html元素错误");
+      Swal.fire("一键评分失败: html元素不匹配");
+      $("#autoRate").prop("value", "一键评分").prop("disabled", false);
     } else {
       var my_rate = Number($("#inputRate")[0].value);
       if (my_rate > 0 && my_rate <= 5 && my_rate % 0.5 == 0) {
         rating(my_rate, 0);
       } else {
         Swal.fire("分数错误：数值必须在0.5-5之间且为0.5的整数倍");
+        $("#autoRate").prop("value", "一键评分").prop("disabled", false);
       }
     }
   }
